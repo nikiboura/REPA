118445121fe796bb8343bd4b6db926d39a18de8e
Niki Boura <nikiboura@Nikis-MacBook-Air.local>
2026-06-15 23:17:03 +0300

    cleanup

---
+++ bash files/prepare_data.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+MAX_SAMPLES=80000
+KAGGLE_JSON="/path/to/kaggle.json"
+
+if [ "$1" == "-1" ]; then
+    MAX_SAMPLES_ARG=""
+else
+    MAX_SAMPLES_ARG="--max-samples $MAX_SAMPLES"
+fi
+
+# Experiment A (SD VAE)
+python dataset_preparation_scripts/prepare_chexpert.py \
+  --chexpert-root ./data/chexpert \
+  --out-dir ./data/chexpert_256_sdvae \
+  --vae-type sd \
+  --resolution 256 \
+  --kaggle-json $KAGGLE_JSON \
+  $MAX_SAMPLES_ARG
+
+# Experiments B & C (MedVAE)
+python dataset_preparation_scripts/prepare_chexpert.py \
+  --chexpert-root ./data/chexpert \
+  --out-dir ./data/chexpert_256_medvae \
+  --vae-type medvae \
+  --resolution 256 \
+  --kaggle-json $KAGGLE_JSON \
+  $MAX_SAMPLES_ARG
